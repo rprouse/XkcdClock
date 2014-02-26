b475716949213073f6725bfe7366c5bdbab4d4ce
Rob Prouse <rob@prouse.org>
2014-02-26 15:46:35 -0500

    Update the clock every 30 seconds

--- XkcdClock/ClockRotation.cs
+++ XkcdClock/ClockRotation.cs
@@ -1,17 +1,25 @@
-﻿// /*--------------------------------------------------------------------------------------+
-// |
-// |     $Source: ClockRotation.cs $
-// |
-// |  $Copyright: (c) 2014 Bentley Systems, Incorporated. All rights reserved. $
-// |
-// +--------------------------------------------------------------------------------------*/
-
-using System;
+﻿using System;
+using System.ComponentModel;
+using System.Timers;
 
 namespace XkcdClock
 {
-    public class ClockRotation
+    public class ClockRotation : INotifyPropertyChanged
     {
+        private readonly Timer _timer;
+
+        public ClockRotation()
+        {
+            // Update the clock every 30 seconds
+            _timer = new Timer()
+            {
+                Interval = 30000,
+                AutoReset = true,
+                Enabled = true
+            };
+            _timer.Elapsed += (sender, e) => OnPropertyChanged( "InnerRotation" );
+        }
+
         /// <summary>
         /// Gets the rotation of the world in the center of the diagram based on the time.
         /// </summary>
@@ -27,6 +35,14 @@ namespace XkcdClock
             var date = DateTime.UtcNow;
             var seconds = new TimeSpan( date.Hour, date.Minute, date.Second ).TotalSeconds;
             return seconds/SECONDS_IN_DAY*360d;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged( string propertyName )
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if ( handler != null ) handler( this, new PropertyChangedEventArgs( propertyName ) );
         }
     }
 }
