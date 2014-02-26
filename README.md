4c29a6fcbb84cb8579045f0afeafe736cb287703
Rob Prouse <rob@prouse.org>
2014-02-26 16:02:45 -0500

    Removed the window chrome. Left click to drag and move the clock. Escape key closes.

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Xkcd World Clock
 
 A WPF desktop implementation of the [World Clock](http://xkcd.com/1335/) created by Randall Munroe, creator of the web comic [xkcd](http://xkcd.com).
 
+- Left Click and drag to move the clock
+- The Escape key will close the clock
+
 ![World Clock](http://imgs.xkcd.com/comics/now.png)
--- XkcdClock/MainWindow.xaml.cs
+++ XkcdClock/MainWindow.xaml.cs
@@ -1,15 +1,27 @@
-﻿using System.Windows;
+﻿using System.Windows.Input;
 
 namespace XkcdClock
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape)
+                Close();
         }
     }
 }
